0efed4aa94e65f8d3d7f9dd0cfb1be1444588f71
pratikshinde84 <144866212+pratikshinde84@users.noreply.github.com>
2024-02-28 01:12:38 +0530

    Final version commit

---
+++ app/src/main/java/com/example/secureserver/SplashActivity.java
@@ -0,0 +1,49 @@
+package com.example.secureserver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import com.example.secureserver.R;
+
+public class SplashActivity extends AppCompatActivity {
+    TextView tv_title,tv_subtitle;
+    Animation fadeInAnim;
+
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        preferences= PreferenceManager.getDefaultSharedPreferences(SplashActivity.this);
+        editor=preferences.edit();
+        tv_title=findViewById(R.id.tv_main_title);
+        tv_subtitle=findViewById(R.id.tv_main_subtitle);
+
+        fadeInAnim= AnimationUtils.loadAnimation(SplashActivity.this, R.anim.fadein);
+
+        tv_title.setAnimation(fadeInAnim);
+        tv_subtitle.setAnimation(fadeInAnim);
+
+        Handler h=new Handler();
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },3500);
+
+    }
+}
