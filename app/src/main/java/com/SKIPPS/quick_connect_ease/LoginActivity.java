1cbfb97e5f84d0312daee093555493870bccbfbd
pratikshinde84 <144866212+pratikshinde84@users.noreply.github.com>
2024-03-26 13:40:55 +0530

    Final version commit

--- app/src/main/java/com/SKIPPS/quick_connect_ease/LoginActivity.java
+++ app/src/main/java/com/SKIPPS/quick_connect_ease/LoginActivity.java
@@ -2,14 +2,28 @@ package com.SKIPPS.quick_connect_ease;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.secureserver.R;
 
 public class LoginActivity extends AppCompatActivity {
+    Button btn_login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btn_login=findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(LoginActivity.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
