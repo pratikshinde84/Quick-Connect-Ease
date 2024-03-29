4e8f20afa75ce0e42e79d59bde7bb345a4a1c4b2
pratikshinde84 <144866212+pratikshinde84@users.noreply.github.com>
2024-03-29 12:06:41 +0530

    Final version commit

--- app/src/main/java/com/SKIPPS/quick_connect_ease/fragment_download_file.java
+++ app/src/main/java/com/SKIPPS/quick_connect_ease/fragment_download_file.java
@@ -14,7 +14,6 @@ public class fragment_download_file extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
