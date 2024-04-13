8a4fd84fea2296081bddbfb010db985c794c21d1
pratikshinde84 <144866212+pratikshinde84@users.noreply.github.com>
2024-04-13 11:16:58 +0530

    Final version commit

--- app/src/main/java/com/SKIPPS/quick_connect_ease/SetPasswordActivity.java
+++ app/src/main/java/com/SKIPPS/quick_connect_ease/SetPasswordActivity.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 
 import com.example.secureserver.R;
@@ -13,72 +17,79 @@ import com.google.android.material.textfield.TextInputLayout;
 
 public class SetPasswordActivity extends AppCompatActivity {
     Button btn_save_password;
-    TextInputLayout et_confirm_password;
     EditText enterPasswordEditText;
-
-    TextInputLayout et_enter_password;
     EditText confirmPasswordEditText;
+    CheckBox cb_confirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_password);
 
-        et_confirm_password = findViewById(R.id.et_confirm_password);
-        enterPasswordEditText = et_confirm_password.getEditText();
-
-        et_enter_password = findViewById(R.id.et_enter_password);
-        confirmPasswordEditText = et_enter_password.getEditText();
+        enterPasswordEditText = findViewById(R.id.et_enter_password_input);
+        confirmPasswordEditText = findViewById(R.id.et_confirm_password_input);
+        cb_confirm=findViewById(R.id.cb_confirm);
 
         btn_save_password = findViewById(R.id.btn_save_password);
 
+        cb_confirm.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked)
+                {
+                    enterPasswordEditText.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    confirmPasswordEditText.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                }
+                else
+                {
+                    enterPasswordEditText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    confirmPasswordEditText.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+            }
+        });
         btn_save_password.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String password = enterPasswordEditText.getText().toString();
                 String confirmPassword = confirmPasswordEditText.getText().toString();
 
-                if (!isPasswordLengthValid(password))
+                if (enterPasswordEditText.getText().toString().isEmpty())
                 {
-                    et_enter_password.setError("Password must be at least 8 characters long.");
+                    enterPasswordEditText.setError("Please enter the password");
                 }
-                else if (!containsLetter(password))
+                else if (password.length() < 8)
                 {
-                    et_enter_password.setError("Password must contain at least one letter.");
+                    enterPasswordEditText.setError("Password must be at least 8 characters long.");
                 }
-                else if (!containsDigit(password))
+                else if (!password.matches(".*[a-zA-Z]+.*"))
                 {
-                    et_enter_password.setError("Password must contain at least one digit.");
-                } else if (!containsSpecialCharacter(password))
+                    enterPasswordEditText.setError("Password must contain at least one letter.");
+                }
+                else if (!password.matches(".*\\d+.*"))
                 {
-                    et_enter_password.setError("Password must contain at least one special character.");
+                    enterPasswordEditText.setError("Password must contain at least one digit.");
+                }
+                else if (!password.matches(".*[@#$%^&+=]+.*"))
+                {
+                    enterPasswordEditText.setError("Password must contain at least one special character.");
+                }
+                else if (confirmPassword.isEmpty())
+                {
+                    confirmPasswordEditText.setError("Please enter the password again");
                 }
                 else if (!password.equals(confirmPassword))
                 {
-                    et_confirm_password.setError("Passwords do not match.");
+                    confirmPasswordEditText.setError("Passwords do not match.");
                 }
                 else
                 {
+                    enterPasswordEditText.setError(null);
+                    confirmPasswordEditText.setError(null);
                     Intent i = new Intent(SetPasswordActivity.this, LoginActivity.class);
                     startActivity(i);
                     finish();
                 }
             }
         });
-    }
-    private boolean isPasswordLengthValid (String password){
-        return password.length() >= 8;
-    }
-
-    private boolean containsLetter (String password){
-        return password.matches(".*[a-zA-Z]+.*");
-    }
-
-    private boolean containsDigit (String password){
-        return password.matches(".*\\d+.*");
-    }
-
-    private boolean containsSpecialCharacter (String password){
-        return password.matches(".*[@#$%^&+=]+.*");
     }
 }
